41c905e9f64676573ba4e4fc32f0c642ab31d1a7
Kevin <kevin.leroux@etu.univ-lehavre.fr>
2022-06-03 11:54:19 +0200

    Modify the number of argument for the function get result

--- ScrappingFiles/Scrapper.py
+++ ScrappingFiles/Scrapper.py
@@ -10,15 +10,16 @@ import time
 class Scraper:
     KEYS = ["resultats-par-niveau-dpt-t1-france-entiere.xlsx", "resultats-par-niveau-reg-t1-france-entiere.xlsx",
         "resultats-par-niveau-dpt-t2-france-entiere.xlsx", "resultats-par-niveau-reg-t2-france-entiere.xlsx"]
-
+    
+    
     def __init__(self, url):
         self.url = url
         
-    def get_result(url,key,result_filename):
+    def get_result(self,id_key,result_filename):
         service = FirefoxService(GeckoDriverManager().install())
         browser = webdriver.Firefox(service=service)
         # get to the url
-        browser.get(url)
+        browser.get(self.url)
         
         #make the servers think it's a human behind
         time.sleep(3)
@@ -27,7 +28,7 @@ class Scraper:
         element = browser.find_element_by_id('resources.search')
         element.clear()
         # enter data in the bar
-        element.send_keys(key)
+        element.send_keys(self.KEYS[id_key])
 
         #press enter (not you , the program)
         element.send_keys(Keys.ENTER)
@@ -35,11 +36,29 @@ class Scraper:
         time.sleep(3)
         download_link=browser.find_element_by_xpath(
             "//a[@title='Télécharger la ressource']").get_attribute("href")
-        complete_filepath= os.path.join("Data/CSV/Results",result_filename)
-        response=request.urlretrieve(download_link,complete_filepath)
+        path=""
         
+        if id_key==0:
+            path="Data/XLSX/Results/Departement/First_Round/"
+            pass
+        if id_key==1:
+            path="Data/XLSX/Results/Region/First_Round/"
+            pass
+        if id_key==2:
+            path="Data/XLSX/Results/Departement/Second_Round/"
+            pass
+        if id_key==3:
+            path="Data/XLSX/Results/Region/Second_Round/"
+            pass
+
+        complete_filepath= os.path.join(path,result_filename)
+        response=request.urlretrieve(download_link,complete_filepath)
+            
         time.sleep(3)
         browser.close()
         browser.quit()
-
+if __name__=="__main__":
+    
+    scrap=Scraper("https://www.data.gouv.fr/fr/datasets/election-presidentielle-des-10-et-24-avril-2022-resultats-definitifs-du-1er-tour/")
+    scrap.get_result()
     
